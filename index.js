5d2a1f974feac3a9432b5d92e34ca064b88fe408
Justyna Wasilewska <malajustine@gmail.com>
2017-09-16 19:14:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
-  if (string.toUpperCase()) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!";
   }
   if (string === "I love you, Grandma.") {
